6f26bd4de444aae2352df78faf96a28946d3d96e
777 <nikitaevstafyev2012@yandex.ru>
2018-11-15 21:00:37 +0300

    [Ten training][Task f]

---
+++ src/main/java/training/ten/f/Main.java
@@ -0,0 +1,11 @@
+package training.ten.f;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Canvas canvas = Canvas.createCanvas();
+        System.out.println("Answer: " + canvas.poorCanvas());
+    }
+}
